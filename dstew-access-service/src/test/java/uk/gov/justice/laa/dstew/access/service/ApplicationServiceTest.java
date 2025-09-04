3bf4497f426927424585496f4ff21768dea33543
jason smallman <jason.smallman@digital.justice.gov.uk>
2025-09-04 14:36:53 +0100

    added unit tests for application trannsformer service

---
+++ dstew-access-service/src/test/java/uk/gov/justice/laa/dstew/access/service/ApplicationServiceTest.java
@@ -0,0 +1,246 @@
+package uk.gov.justice.laa.dstew.access.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.justice.laa.dstew.access.config.SqsProducer;
+import uk.gov.justice.laa.dstew.access.entity.ApplicationEntity;
+import uk.gov.justice.laa.dstew.access.entity.ApplicationHistoryEntity;
+import uk.gov.justice.laa.dstew.access.entity.ApplicationProceedingEntity;
+import uk.gov.justice.laa.dstew.access.entity.EmbeddedRecordHistoryEntity;
+import uk.gov.justice.laa.dstew.access.exception.ApplicationNotFoundException;
+import uk.gov.justice.laa.dstew.access.mapper.ApplicationMapper;
+import uk.gov.justice.laa.dstew.access.model.*;
+import uk.gov.justice.laa.dstew.access.repository.ApplicationHistoryRepository;
+import uk.gov.justice.laa.dstew.access.repository.ApplicationRepository;
+import uk.gov.justice.laa.dstew.access.validation.ApplicationValidations;
+
+import java.time.Instant;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplicationServiceTest {
+
+    @InjectMocks
+    private ApplicationService classUnderTest;
+
+    @Mock
+    private ApplicationRepository repository;
+    @Mock
+    private ApplicationHistoryRepository history;
+    @Mock
+    private ApplicationMapper mapper;
+    @Mock
+    private ApplicationValidations validator;
+    @Mock
+    private SqsProducer sqsProducer;
+    @Mock
+    private ObjectMapper objectMapper;
+
+    @Test
+    void shouldThrowExceptionWhenApplicationHistoryNotFound() {
+        ApplicationUpdateRequest request = new ApplicationUpdateRequest();
+        request.setIsEmergencyApplication(true);
+        request.setClientId(UUID.randomUUID());
+
+        when(repository.findById(any())).thenThrow(ApplicationNotFoundException.class);
+
+        assertThrows(ApplicationNotFoundException.class,
+                () -> classUnderTest.getApplicationsLatestHistory(UUID.randomUUID()));
+
+    }
+
+    @Test
+    void shouldGetApplicationsLatestHistory() {
+
+        UUID applicationId = UUID.randomUUID();
+
+        EmbeddedRecordHistoryEntity recordHistoryEntity = new EmbeddedRecordHistoryEntity();
+        recordHistoryEntity.setUpdatedAt(Instant.now());
+        recordHistoryEntity.setUpdatedBy("user");
+
+        ApplicationHistoryEntity foundEntity = new ApplicationHistoryEntity();
+        foundEntity.setApplicationId(applicationId);
+        foundEntity.setAction("CREATED");
+        foundEntity.setId(UUID.randomUUID());
+
+        ApplicationHistory mappedHistory = new ApplicationHistory();
+        mappedHistory.setApplicationId(applicationId);
+        mappedHistory.setAction(ActionType.CREATED);
+        mappedHistory.setId(UUID.randomUUID());
+
+        ApplicationEntity foundApplicationEntity = new ApplicationEntity();
+        foundApplicationEntity.setIsEmergencyApplication(true);
+        foundApplicationEntity.setClientId(UUID.randomUUID());
+        foundEntity.setId(applicationId);
+
+        when(repository.findById(any())).thenReturn(Optional.of(foundApplicationEntity));
+        when(history.findFirstByApplicationIdOrderByTimestampDesc(any())).thenReturn(Optional.of(foundEntity));
+        when(mapper.toApplicationHistory(any())).thenReturn(mappedHistory);
+
+        ApplicationHistory result = classUnderTest.getApplicationsLatestHistory(applicationId);
+        assertThat(result.getApplicationId()).isEqualTo(mappedHistory.getApplicationId());
+        assertThat(result.getAction()).isEqualTo(ActionType.CREATED);
+        assertThat(result.getId()).isEqualTo(mappedHistory.getId());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenApplicationNotFound() {
+        ApplicationEntity updateEntity = new ApplicationEntity();
+        updateEntity.setId(UUID.randomUUID());
+
+        ApplicationUpdateRequest request = new ApplicationUpdateRequest();
+        request.setIsEmergencyApplication(true);
+        request.setClientId(UUID.randomUUID());
+
+        when(repository.findById(any())).thenThrow(ApplicationNotFoundException.class);
+
+        assertThrows(ApplicationNotFoundException.class,
+                () -> classUnderTest.updateApplication(updateEntity.getId(), request));
+
+    }
+
+    @Test
+    void shouldUpdateApplications() {
+
+        UUID applicationId = UUID.randomUUID();
+
+        ApplicationUpdateRequest request = new ApplicationUpdateRequest();
+        request.setIsEmergencyApplication(true);
+        request.setClientId(UUID.randomUUID());
+
+        EmbeddedRecordHistoryEntity recordHistoryEntity = new EmbeddedRecordHistoryEntity();
+        recordHistoryEntity.setUpdatedAt(Instant.now());
+        recordHistoryEntity.setUpdatedBy("user");
+
+        ApplicationEntity foundEntity = new ApplicationEntity();
+        foundEntity.setIsEmergencyApplication(request.getIsEmergencyApplication());
+        foundEntity.setClientId(request.getClientId());
+        foundEntity.setId(applicationId);
+        foundEntity.setRecordHistory(recordHistoryEntity);
+
+        ApplicationEntity savedEntity = new ApplicationEntity();
+        savedEntity.setIsEmergencyApplication(foundEntity.getIsEmergencyApplication());
+        savedEntity.setClientId(foundEntity.getClientId());
+        savedEntity.setId(applicationId);
+
+        Map<String, Object> historicSnapshot =  new HashMap<>();
+        historicSnapshot.put("field", "value");
+
+        doNothing().when(validator).checkApplicationUpdateRequest(any(), any());
+        doNothing().when(mapper).updateApplicationEntity(any(ApplicationEntity.class), any(ApplicationUpdateRequest.class));
+        when(repository.findById(any())).thenReturn(Optional.of(foundEntity));
+        when(repository.save(any())).thenReturn(savedEntity);
+        when(objectMapper.convertValue(any(), any(TypeReference.class))).thenReturn(historicSnapshot);
+        doNothing().when(sqsProducer).createHistoricRecord(any());
+        classUnderTest.updateApplication(applicationId, request);
+        verify(sqsProducer).createHistoricRecord(any());
+    }
+
+    @Test
+    void shouldCreateApplication() {
+        ApplicationCreateRequest request = new ApplicationCreateRequest();
+        request.setIsEmergencyApplication(true);
+        request.setClientId(UUID.randomUUID());
+
+        ApplicationEntity mappedEntity = new ApplicationEntity();
+        mappedEntity.setIsEmergencyApplication(request.getIsEmergencyApplication());
+        mappedEntity.setClientId(request.getClientId());
+
+        EmbeddedRecordHistoryEntity recordHistoryEntity = new EmbeddedRecordHistoryEntity();
+        recordHistoryEntity.setCreatedAt(Instant.now());
+        ApplicationEntity savedEntity = new ApplicationEntity();
+        savedEntity.setIsEmergencyApplication(mappedEntity.getIsEmergencyApplication());
+        savedEntity.setClientId(mappedEntity.getClientId());
+        savedEntity.setRecordHistory(recordHistoryEntity);
+        savedEntity.setId(UUID.randomUUID());
+
+        Map<String, Object> historicSnapshot =  new HashMap<>();
+        historicSnapshot.put("field", "value");
+
+        doNothing().when(validator).checkApplicationCreateRequest(any());
+        when(mapper.toApplicationEntity(any())).thenReturn(mappedEntity);
+        when(repository.save(any())).thenReturn(savedEntity);
+        when(objectMapper.convertValue(any(), any(TypeReference.class))).thenReturn(historicSnapshot);
+        doNothing().when(sqsProducer).createHistoricRecord(any());
+
+        UUID result = classUnderTest.createApplication(request);
+
+        assertThat(result).isNotNull();
+        assertThat(result).isEqualTo(savedEntity.getId());
+    }
+
+    @Test
+    void shouldGetAllApplications() {
+        ApplicationEntity firstEntity = new ApplicationEntity();
+        firstEntity.setIsEmergencyApplication(true);
+        firstEntity.setId(UUID.randomUUID());
+        firstEntity.setProceedings(List.of(new ApplicationProceedingEntity(), new ApplicationProceedingEntity()));
+
+        Application firstApplication = new Application();
+        firstApplication.setId(firstEntity.getId());
+        firstApplication.setIsEmergencyApplication(firstEntity.getIsEmergencyApplication());
+        firstApplication.setProceedings(List.of(new ApplicationProceeding(), new ApplicationProceeding()));
+
+        ApplicationEntity secondEntity = new ApplicationEntity();
+        secondEntity.setIsEmergencyApplication(false);
+        secondEntity.setId(UUID.randomUUID());
+        secondEntity.setProceedings(List.of(new ApplicationProceedingEntity()));
+
+        Application secondApplication = new Application();
+        secondApplication.setId(secondEntity.getId());
+        secondApplication.setIsEmergencyApplication(secondEntity.getIsEmergencyApplication());
+        secondApplication.setProceedings(List.of(new ApplicationProceeding()));
+
+        when(repository.findAll()).thenReturn(List.of(firstEntity, secondEntity));
+        when(mapper.toApplication(firstEntity)).thenReturn(firstApplication);
+        when(mapper.toApplication(secondEntity)).thenReturn(secondApplication);
+
+        List<Application> result = classUnderTest.getAllApplications();
+
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0).getId()).isEqualTo(firstEntity.getId());
+        assertThat(result.get(0).getProceedings()).hasSize(2);
+        assertThat(result.get(1).getId()).isEqualTo(secondEntity.getId());
+        assertThat(result.get(1).getProceedings()).hasSize(1);
+
+    }
+
+    @Test
+    void shouldGetApplicationById() {
+        ApplicationEntity firstEntity = new ApplicationEntity();
+        firstEntity.setId(UUID.randomUUID());
+        firstEntity.setProceedings(List.of(new ApplicationProceedingEntity()));
+        Application firstApplication = new Application();
+        firstApplication.setId(firstEntity.getId());
+
+        when(repository.findById(any())).thenReturn(Optional.of(firstEntity));
+        when(mapper.toApplication(firstEntity)).thenReturn(firstApplication);
+
+        Application result = classUnderTest.getApplication(firstEntity.getId());
+
+        assertThat(result).isEqualTo(firstApplication);
+        assertThat(result.getId()).isEqualTo(firstEntity.getId());
+    }
+
+    @Test
+    void shouldThrowExceptionIfApplicationNotFound() {
+        ApplicationEntity firstEntity = new ApplicationEntity();
+        firstEntity.setId(UUID.randomUUID());
+        firstEntity.setProceedings(List.of(new ApplicationProceedingEntity()));
+
+        when(repository.findById(any())).thenThrow(ApplicationNotFoundException.class);
+
+        assertThrows(ApplicationNotFoundException.class,
+                () -> classUnderTest.getApplication(firstEntity.getId()));
+    }
+}
---
+++ dstew-access-service/src/test/java/uk/gov/justice/laa/dstew/access/transformation/ApplicationTransformerTest.java
@@ -0,0 +1,36 @@
+package uk.gov.justice.laa.dstew.access.transformation;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.justice.laa.dstew.access.model.Application;
+import uk.gov.justice.laa.dstew.access.shared.security.EffectiveAuthorizationProvider;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplicationTransformerTest {
+
+    @InjectMocks
+    ApplicationTransformer classUnderTest;
+
+    @Mock
+    EffectiveAuthorizationProvider mockEntra;
+
+    @Test
+    void shouldTransformApplication() {
+        Application request = Application.builder()
+                .id(UUID.randomUUID())
+                .build();
+
+        when(mockEntra.hasAppRole("ProceedingReader")).thenReturn(true);
+
+        Application response = classUnderTest.transform(request);
+        assertThat(response.getId()).isEqualTo(request.getId());
+    }
+}
