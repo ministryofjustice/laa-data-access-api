89d3cadf26d5aa89c25c4a466a29e9c758c2ec3e
Jason Smallman <Jason.smallman@digital.justice.gov.uk>
2025-09-11 16:23:16 +0100

    remove application history (#3)

--- data-access-service/src/main/resources/db/migration/V1__set_up_schema.sql
+++ data-access-service/src/main/resources/db/migration/V1__set_up_schema.sql
@@ -32,18 +32,3 @@ CREATE TABLE applications_proceedings (
                                           CONSTRAINT fk_application FOREIGN KEY (application_id)
                                               REFERENCES applications(id) ON DELETE CASCADE
 );
-
--- Application History Table
-CREATE TABLE application_history (
-                                     id UUID PRIMARY KEY,
-                                     application_id UUID NOT NULL,
-                                     user_id VARCHAR(255) NOT NULL,
-                                     resource_type_changed VARCHAR(100) NOT NULL,
-                                     action VARCHAR(100) NOT NULL,
-                                     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                     historic_snapshot JSONB NULL, -- was NOT NULL but initial data load failed
-                                     application_snapshot JSONB NOT NULL,
-
-                                     CONSTRAINT fk_application_history_application FOREIGN KEY (application_id)
-                                         REFERENCES applications(id) ON DELETE CASCADE
-);
