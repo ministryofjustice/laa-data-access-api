3bb2eac7e5953a109f7c1c9a3a27f4dcf96c15aa
daniel.barnett <b4barnett@googlemail.com>
2025-10-20 15:00:16 +0100

    create application table script

---
+++ data-access-service/src/main/resources/db/migration/application_table_create_script.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS application(
+  ID UUID NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT(NOW() AT TIME ZONE 'UTC'),
+  modified_at TIMESTAMPTZ NOT NULL DEFAULT(NOW() AT TIME ZONE 'UTC'),
+  status_id UUID NOT NULL,
+  application_content JSONB NOT NULL,
+  schema_version INTEGER,
+  CONSTRAINT application_pkey PRIMARY KEY (ID),
+  CONSTRAINT application_status_code_lookup_fkey FOREIGN KEY(status_id) REFERENCES status_code_lookup(ID)
+)
+
+-- INSERT INTO public.application(
+-- 	id, status_id, application_content, schema_version)
+-- 	VALUES ('e32a59fc-94ba-44ed-a74e-2296b8d56716', 'c54cf397-e574-4bb3-b6b8-80548f87499a', '{}', 1);
