bbf37e733bdc8a75aae467b707f7481a4a649ca9
daniel.barnett <b4barnett@googlemail.com>
2025-11-06 17:29:37 +0000

    remove unused imports

--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/exception/GlobalExceptionHandler.java
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package uk.gov.justice.laa.dstew.access.exception;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
