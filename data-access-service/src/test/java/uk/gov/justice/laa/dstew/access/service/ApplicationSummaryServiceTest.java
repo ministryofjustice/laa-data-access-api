e6868133a4812ab9525cee8d667927c4029b0c47
jason smallman <jason.smallman@digital.justice.gov.uk>
2025-11-10 09:22:53 +0000

    updated unit test

--- data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/service/ApplicationSummaryServiceTest.java
+++ data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/service/ApplicationSummaryServiceTest.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import uk.gov.justice.laa.dstew.access.entity.ApplicationSummaryEntity;
 import uk.gov.justice.laa.dstew.access.entity.StatusCodeLookupEntity;
-import uk.gov.justice.laa.dstew.access.mapper.ApplicationMapper;
 import uk.gov.justice.laa.dstew.access.mapper.ApplicationSummaryMapper;
 import uk.gov.justice.laa.dstew.access.model.ApplicationSummary;
 import uk.gov.justice.laa.dstew.access.repository.ApplicationSummaryRepository;
--- data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/specification/ApplicationSummarySpecificationTest.java
+++ data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/specification/ApplicationSummarySpecificationTest.java
@@ -2,6 +2,7 @@ package uk.gov.justice.laa.dstew.access.specification;
 
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,18 +10,29 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.jpa.domain.Specification;
 import uk.gov.justice.laa.dstew.access.entity.ApplicationSummaryEntity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 public class ApplicationSummarySpecificationTest {
+    @SuppressWarnings("unchecked")
+    private final Root<ApplicationSummaryEntity> root = mock(Root.class);
+
+    private final CriteriaQuery<?> query = mock(CriteriaQuery.class);
+    private final CriteriaBuilder builder = mock(CriteriaBuilder.class);
 
     @Test
-    void shouldisStatus(){
-        /*
-        Specification<ApplicationSummaryEntity> spec = ApplicationSummarySpecification.isStatus("ACTIVE");
-        Root<ApplicationSummaryEntity> root = null;
-        CriteriaQuery<ApplicationSummaryEntity> query = null;
-        CriteriaBuilder builder = null;
-        spec.toPredicate(root, query, builder);
+    void shouldNotFailWhenIsStatusFilter(){
 
-         */
+        Specification<ApplicationSummaryEntity> spec = ApplicationSummarySpecification.isStatus("ACTIVE");
+        Predicate summaryPredicate = mock(Predicate.class);
+        when(root.get("statusCodeLookupEntity")).thenReturn(mock(jakarta.persistence.criteria.Path.class));
+        when(builder.equal(any(), any())).thenReturn(summaryPredicate);
+
+        Predicate result = spec.toPredicate(root, query, builder);
+
+        assertThat(result).isNotNull();
     }
 }
