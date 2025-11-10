69dbfcdda19daaf44704743d9d3ae8c41b0bad48
jason smallman <jason.smallman@digital.justice.gov.uk>
2025-11-10 12:31:57 +0000

    add application reference specification and test to simplify

--- data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/specification/ApplicationSummarySpecificationTest.java
+++ data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/specification/ApplicationSummarySpecificationTest.java
@@ -9,6 +9,7 @@ import uk.gov.justice.laa.dstew.access.entity.ApplicationSummaryEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -16,68 +17,45 @@ import static org.mockito.Mockito.when;
 public class ApplicationSummarySpecificationTest {
     @SuppressWarnings("unchecked")
     private final Root<ApplicationSummaryEntity> root = mock(Root.class);
+
+    @SuppressWarnings("unchecked")
+    private final Path<Object> rootResults = mock(jakarta.persistence.criteria.Path.class);
+
     private final CriteriaQuery<?> query = mock(CriteriaQuery.class);
     private final CriteriaBuilder builder = mock(CriteriaBuilder.class);
 
 
     @Test
     void shouldNotFailWhenIsStatusFilter(){
-    /*
+
         Specification<ApplicationSummaryEntity> spec = ApplicationSummarySpecification.isStatus("ACTIVE");
 
         Predicate summaryPredicate = mock(Predicate.class);
 
         when(root.get("statusCodeLookupEntity"))
                 .thenReturn(mock(jakarta.persistence.criteria.Path.class));
-        when(statusCodeEntityPath.get("code"))
+
+        when(rootResults.get("code"))
                 .thenReturn(mock(jakarta.persistence.criteria.Path.class));
 
-        when(builder.equal(any(), any())).thenReturn(summaryPredicate);
+        when(builder.equal(any(), eq("ACTIVE"))).thenReturn(summaryPredicate);
 
         Predicate result = spec.toPredicate(root, query, builder);
 
         assertThat(result).isNotNull();
 
-     */
     }
 
     @Test
     void shouldNotFailWhenIsApplicationReferenceFilter(){
         Specification<ApplicationSummaryEntity> spec = ApplicationSummarySpecification.isApplicationReference("ref1");
-
         Predicate summaryPredicate = mock(Predicate.class);
 
         when(root.get("applicationReference"))
                 .thenReturn(mock(jakarta.persistence.criteria.Path.class));
-
-        when(builder.equal(any(), any())).thenReturn(summaryPredicate);
+        when(builder.equal(any(), eq("ref1"))).thenReturn(summaryPredicate);
 
         Predicate result = spec.toPredicate(root, query, builder);
-
         assertThat(result).isNotNull();
     }
-    /*
-      public static Specification<Submission> submissionIdEqualTo(final String submissionId) {
-    return (root, query, cb) ->
-        Optional.ofNullable(submissionId).isPresent()
-            ? cb.equal(root.get("id"), UUID.fromString(submissionId))
-            : cb.conjunction();
-  }
-
-    @Test
-  void shouldBuildSpecificationWithSubmissionId() {
-    UUID submissionId = Uuid7.timeBasedUuid();
-    Specification<Submission> spec =
-        SubmissionSpecification.submissionIdEqualTo(submissionId.toString());
-
-    Predicate withSubmissionId = Mockito.mock(Predicate.class);
-
-    Mockito.when(root.get("id")).thenReturn(Mockito.mock(jakarta.persistence.criteria.Path.class));
-    Mockito.when(cb.equal(Mockito.any(), Mockito.eq(submissionId))).thenReturn(withSubmissionId);
-
-    var actualResults = spec.toPredicate(root, query, cb);
-
-    assertThat(actualResults).isNotNull();
-  }
-     */
 }
