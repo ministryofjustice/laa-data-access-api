01d2818275d9ee8fe9824e6c185eeda7efffadbd
jason smallman <jason.smallman@digital.justice.gov.uk>
2025-11-12 10:29:06 +0000

    removed status lookup data creation

--- data-access-service/src/main/resources/db/migration/V1_1_2__insert_status_code_lookup.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO status_code_lookup(ID, code, description) VALUES('019a0c3d-ddac-7f2e-ae26-6c442956a801','IN_PROGRESS','description for IN_PROGRESS');
-INSERT INTO status_code_lookup(ID, code, description) VALUES('019a0c3d-ddac-7f2e-ae26-6c442956a802','SUBMITTED','description for SUBMITTED');
