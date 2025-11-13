6f864828b480329f19972b79d6a6b3e0b2b30cb1
Jason Smallman <Jason.smallman@digital.justice.gov.uk>
2025-11-13 10:53:58 +0000

    Feature/dstew 669 using specification (#63)
    * DSTEW-308: Vuln Scanning Deploys
    
    * added specification setup
    
    * added test
    
    * changed filter
    
    * updated unit test
    
    * add application reference specification and test to simplify
    
    * add application reference specification and test to simplify
    
    * add specification and status enum
    
    * add specification and status enum
    
    * add specification and status enum
    
    * removed findall null check as will not happen
    
    * added status lookup data creation
    
    * removed status lookup data creation
    
    * changes in response to comments
    
    * changes in response to comments
    
    ---------
    
    Co-authored-by: Peter Hunt <naseberry@users.noreply.github.com>

--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/service/ApplicationSummaryService.java
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/service/ApplicationSummaryService.java
@@ -1,13 +1,18 @@
 package uk.gov.justice.laa.dstew.access.service;
 
+import java.util.Collections;
 import java.util.List;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import uk.gov.justice.laa.dstew.access.entity.ApplicationSummaryEntity;
 import uk.gov.justice.laa.dstew.access.mapper.ApplicationSummaryMapper;
+import uk.gov.justice.laa.dstew.access.model.ApplicationStatus;
 import uk.gov.justice.laa.dstew.access.model.ApplicationSummary;
 import uk.gov.justice.laa.dstew.access.repository.ApplicationSummaryRepository;
+import uk.gov.justice.laa.dstew.access.specification.ApplicationSummarySpecification;
 
 /**
  * Service class for handling application summary requests.
@@ -19,10 +24,10 @@ public class ApplicationSummaryService {
 
   public ApplicationSummaryService(
         final ApplicationSummaryRepository applicationSummaryRepository,
-        final ApplicationSummaryMapper applicationMapper
+        final ApplicationSummaryMapper applicationSummaryMapper
   ) {
     this.applicationSummaryRepository = applicationSummaryRepository;
-    this.mapper = applicationMapper;
+    this.mapper = applicationSummaryMapper;
   }
 
   /**
@@ -31,11 +36,15 @@ public class ApplicationSummaryService {
    * @return the list of applications
    */
   @PreAuthorize("@entra.hasAppRole('ApplicationReader')")
-  public List<ApplicationSummary> getAllApplications(String applicationStatus, Integer page, Integer pageSize) {
+  public List<ApplicationSummary> getAllApplications(ApplicationStatus applicationStatus, Integer page, Integer pageSize) {
     Pageable pageDetails = PageRequest.of(page, pageSize);
 
-    return applicationSummaryRepository.findByStatusCodeLookupEntity_Code(applicationStatus, pageDetails)
-            .stream().map(mapper::toApplicationSummary).toList();
+    Page<ApplicationSummaryEntity> applicationSummaryPage = applicationSummaryRepository
+            .findAll(ApplicationSummarySpecification.isStatus(applicationStatus), pageDetails);
+
+    List<ApplicationSummaryEntity> allApplications = applicationSummaryPage.getContent();
+
+    return allApplications.stream().map(mapper::toApplicationSummary).toList();
   }
 
   /**
@@ -44,8 +53,8 @@ public class ApplicationSummaryService {
    * @return the total of list of applications
    */
   @PreAuthorize("@entra.hasAppRole('ApplicationReader')")
-  public Integer getAllApplicationsTotal(String applicationStatus) {
-    return applicationSummaryRepository.countByStatusCodeLookupEntity_Code(applicationStatus);
+  public long getAllApplicationsTotal(ApplicationStatus applicationStatus) {
+    return applicationSummaryRepository.count(ApplicationSummarySpecification.isStatus(applicationStatus));
   }
 
 }
---
+++ data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/specification/ApplicationSummarySpecificationTest.java
@@ -0,0 +1,49 @@
+package uk.gov.justice.laa.dstew.access.specification;
+
+import jakarta.persistence.criteria.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.jpa.domain.Specification;
+import uk.gov.justice.laa.dstew.access.entity.ApplicationSummaryEntity;
+import uk.gov.justice.laa.dstew.access.model.ApplicationStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplicationSummarySpecificationTest {
+    @SuppressWarnings("unchecked")
+    private final Root<ApplicationSummaryEntity> root = mock(Root.class);
+
+    @SuppressWarnings("unchecked")
+    private final Path<Object> rootResults = mock(jakarta.persistence.criteria.Path.class);
+
+    private final CriteriaQuery<?> query = mock(CriteriaQuery.class);
+    private final CriteriaBuilder builder = mock(CriteriaBuilder.class);
+
+
+    @Test
+    void shouldNotFailWhenIsStatusFilter(){
+
+        Specification<ApplicationSummaryEntity> spec = ApplicationSummarySpecification.isStatus(ApplicationStatus.IN_PROGRESS);
+
+        Predicate summaryPredicate = mock(Predicate.class);
+
+        when(root.get("statusCodeLookupEntity"))
+                .thenReturn(mock(jakarta.persistence.criteria.Path.class));
+
+        when(rootResults.get("code"))
+                .thenReturn(mock(jakarta.persistence.criteria.Path.class));
+
+        when(builder.equal(any(), eq("IN_PROGRESS"))).thenReturn(summaryPredicate);
+
+        Predicate result = spec.toPredicate(root, query, builder);
+
+        assertThat(result).isNotNull();
+
+    }
+}
