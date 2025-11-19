2b543870010b6c7a071f637a7564c99cfa162707
carl.mcintyre <carl.mcintyre@justice.gov.uk>
2025-11-19 10:54:01 +0000

    added _andNoRepository checks in negative create scenarios added repository get and save checks

---
+++ data-access-service/src/integrationTest/java/uk/gov/justice/laa/dstew/access/repository/ApplicationTest.java
@@ -0,0 +1,24 @@
+package uk.gov.justice.laa.dstew.access.repository;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.justice.laa.dstew.access.entity.ApplicationEntity;
+import uk.gov.justice.laa.dstew.access.utils.BaseIntegrationTest;
+
+import static uk.gov.justice.laa.dstew.access.utils.asserters.ApplicationAsserts.assertApplicationEqual;
+
+public class ApplicationTest extends BaseIntegrationTest {
+
+    @Test
+    public void givenSaveOfExpectedApplication_whenGetCalled_expectedAndActualAreEqual() {
+
+        // given
+        ApplicationEntity expected = applicationFactory.create();
+        applicationRepository.save(expected);
+
+        // when
+        ApplicationEntity actual = applicationRepository.findById(expected.getId()).orElse(null);
+
+        // then
+        assertApplicationEqual(expected, actual);
+    }
+}
