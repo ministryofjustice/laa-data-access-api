b0a99b32a89a14606f50325276b7000297d879a3
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2025-11-24 16:12:14 +0000

    Drop applications table, fix previous copy-paste mistake

--- data-access-service/src/main/resources/db/migration/V1_1_6_clean_up_database.sql
+++ data-access-service/src/main/resources/db/migration/V1_1_6_clean_up_database.sql
@@ -3,3 +3,6 @@ DROP TABLE IF EXISTS status_code_lookup CASCADE;
 
 -- Drop application proceedings tables if they exist
 DROP TABLE IF EXISTS applications_proceedings CASCADE;
+
+-- Drop applications if it still exists
+DROP TABLE IF EXISTS applications CASCADE;
