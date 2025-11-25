09bcd68f0ccaeb8992fe4da09badf6741c2b2a97
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2025-11-25 10:45:16 +0000

    chore: DSTEW-796 Delete unused tables (#83)
    * chore: DSTEW-796 Delete unused tables

---
+++ data-access-service/src/main/resources/db/migration/V1_1_6_clean_up_database.sql
@@ -0,0 +1,8 @@
+-- Drop status lookup table if it still exists
+DROP TABLE IF EXISTS status_code_lookup CASCADE;
+
+-- Drop application proceedings tables if they exist
+DROP TABLE IF EXISTS applications_proceedings CASCADE;
+
+-- Drop applications if it still exists
+DROP TABLE IF EXISTS applications CASCADE;
