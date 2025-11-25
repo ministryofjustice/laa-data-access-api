f4c86f38bebd1de6148e8d62191f871870fbc426
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2025-11-25 15:29:54 +0000

    Added repeatable test data migraation

---
+++ data-access-service/src/main/resources/db/migration/R__insert_test_data.sql
@@ -0,0 +1,11 @@
+-- R__insert_test_data.sql
+INSERT INTO caseworker (username) VALUES ('caseworker1');
+INSERT INTO caseworker (username) VALUES ('caseworker2');
+INSERT INTO caseworker (username) VALUES ('caseworker3');
+INSERT INTO caseworker (username) VALUES ('caseworker4');
+INSERT INTO caseworker (username) VALUES ('caseworker5');
+INSERT INTO caseworker (username) VALUES ('caseworker6');
+INSERT INTO caseworker (username) VALUES ('caseworker7');
+INSERT INTO caseworker (username) VALUES ('caseworker8');
+INSERT INTO caseworker (username) VALUES ('caseworker9');
+INSERT INTO caseworker (username) VALUES ('caseworker10');
