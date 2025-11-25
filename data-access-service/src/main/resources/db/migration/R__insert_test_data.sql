90557f0f4efc219e265820905d689da0b0c67db1
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2025-11-25 17:05:30 +0000

    feat: DSTEW-831 Add caseworker entity (#85)
    * feat: DSTEW-831 Add case worker entity and table

---
+++ data-access-service/src/main/resources/db/migration/R__insert_test_data.sql
@@ -0,0 +1,11 @@
+-- R__insert_test_data.sql
+INSERT INTO caseworkers (username) VALUES ('caseworker1');
+INSERT INTO caseworkers (username) VALUES ('caseworker2');
+INSERT INTO caseworkers (username) VALUES ('caseworker3');
+INSERT INTO caseworkers (username) VALUES ('caseworker4');
+INSERT INTO caseworkers (username) VALUES ('caseworker5');
+INSERT INTO caseworkers (username) VALUES ('caseworker6');
+INSERT INTO caseworkers (username) VALUES ('caseworker7');
+INSERT INTO caseworkers (username) VALUES ('caseworker8');
+INSERT INTO caseworkers (username) VALUES ('caseworker9');
+INSERT INTO caseworkers (username) VALUES ('caseworker10');
