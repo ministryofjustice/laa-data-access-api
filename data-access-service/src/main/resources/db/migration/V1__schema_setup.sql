ded5e409d341d18f5e48b9ecb7fd5a119d9654b3
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2025-11-27 12:06:04 +0000

    Migrations updated to include recent columns

--- data-access-service/src/main/resources/db/migration/V1__schema_setup.sql
+++ data-access-service/src/main/resources/db/migration/V1__schema_setup.sql
@@ -2,31 +2,17 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 ---------------------------------------------------------------------
--- APPLICATIONS
----------------------------------------------------------------------
-CREATE TABLE applications (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    application_reference VARCHAR,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    modified_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
-
-    status VARCHAR(20) NOT NULL,
-    application_content JSONB NOT NULL,
-    schema_version INTEGER
-);
-
----------------------------------------------------------------------
 -- INDIVIDUALS
 ---------------------------------------------------------------------
 CREATE TABLE individuals (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
-    modified_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+     created_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+     modified_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
 
-    first_name VARCHAR NOT NULL,
-    last_name VARCHAR NOT NULL,
-    date_of_birth DATE NOT NULL,
-    individual_content JSONB NOT NULL
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     date_of_birth DATE NOT NULL,
+     individual_content JSONB NOT NULL
 );
 
 ---------------------------------------------------------------------
@@ -36,7 +22,26 @@ CREATE TABLE caseworkers(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     created_at TIMESTAMPTZ NOT NULL DEFAULT(NOW() AT TIME ZONE 'UTC'),
     modified_at TIMESTAMPTZ NOT NULL DEFAULT(NOW() AT TIME ZONE 'UTC'),
+
     username VARCHAR NOT NULL
+);
+
+---------------------------------------------------------------------
+-- APPLICATIONS
+---------------------------------------------------------------------
+CREATE TABLE applications (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+    modified_at TIMESTAMPTZ NOT NULL DEFAULT (NOW() AT TIME ZONE 'UTC'),
+
+    caseworker_id UUID NULL,
+    application_reference VARCHAR,
+    status VARCHAR(20) NOT NULL,
+    application_content JSONB NOT NULL,
+    schema_version INTEGER
+
+    CONSTRAINT fk_application_caseworker FOREIGN KEY (caseworker_id)
+        REFERENCES caseworkers(id)
 );
 
 ---------------------------------------------------------------------
