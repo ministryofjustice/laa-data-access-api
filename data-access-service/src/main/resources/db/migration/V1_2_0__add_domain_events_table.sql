2f13984d05c6e5c82f048df5f277a0716df9fc8e
Jason Smallman <Jason.smallman@digital.justice.gov.uk>
2025-12-03 15:27:57 +0000

    Feature/dstew 899 flyway version change (#99)
    * DSTEW-308: Vuln Scanning Deploys
    
    * added additional tests for new specification
    
    * Added domain_Events entities and datebase script to support domain_events
    
    * Added domain_Events entities and database script to support domain_events
    
    * Amended domain_Events entity to only store id of caseworker and application
    
    * moved DomainModelType to new model
    
    * reversion of 1_1_10 to 1_2_0
    
    * reversion of 1_1_10 to 1_2_0
    
    * reversion of 1_1_10 to 1_2_0
    
    ---------
    
    Co-authored-by: Peter Hunt <naseberry@users.noreply.github.com>

--- data-access-service/src/main/resources/db/migration/V1_2_0__add_domain_events_table.sql
+++ data-access-service/src/main/resources/db/migration/V1_2_0__add_domain_events_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS domain_events(
     caseworker_id UUID NULL,
     type VARCHAR(50) NOT NULL,
     data JSONB NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT(NOW() AT TIME ZONE 'UTC'),
+    created_at TIMESTAMPTZ NOT NULL  DEFAULT(NOW() AT TIME ZONE 'UTC'),
     created_by VARCHAR(255) NOT NULL,
     CONSTRAINT domain_events_pkey PRIMARY KEY (ID),
     CONSTRAINT domain_events_application_id_fkey FOREIGN KEY(application_id) REFERENCES application(id),
