a04f5be8328327c12384619a46f61310dd2e5746
Jason Smallman <Jason.smallman@digital.justice.gov.uk>
2025-12-03 15:14:34 +0000

    Feature/dstew 899 (#98)
    * DSTEW-308: Vuln Scanning Deploys
    
    * added additional tests for new specification
    
    * Added domain_Events entities and datebase script to support domain_events
    
    * Added domain_Events entities and database script to support domain_events
    
    * Amended domain_Events entity to only store id of caseworker and application
    
    * moved DomainModelType to new model
    
    ---------
    
    Co-authored-by: Peter Hunt <naseberry@users.noreply.github.com>

---
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/entity/DomainEventEntity.java
@@ -0,0 +1,54 @@
+package uk.gov.justice.laa.dstew.access.entity;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.vladmihalcea.hibernate.type.json.JsonType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
+import uk.gov.justice.laa.dstew.access.model.DomainEventType;
+
+/**
+ * Represents a domain events table.
+ */
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "domain_events")
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class DomainEventEntity {
+  @Id
+  @Column(name = "id")
+  private UUID id;
+
+  @Column(name = "application_id")
+  private UUID applicationId;
+
+  @Column(name = "caseworker_id")
+  private UUID caseWorkerId;
+
+  @Column(name = "type", nullable = false)
+  @Enumerated(EnumType.STRING)
+  private DomainEventType type;
+
+  @Type(JsonType.class)
+  @Column(columnDefinition = "jsonb", nullable = false)
+  private Map<String, Object> data;
+
+  @Column(name = "created_at")
+  private Instant createdAt;
+
+  @Column(name = "created_by")
+  private String createdBy;
+}
---
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/model/DomainEventType.java
@@ -0,0 +1,9 @@
+package uk.gov.justice.laa.dstew.access.model;
+
+/**
+ * Represents a domain events type.
+ */
+public enum DomainEventType {
+    ASSIGN_APPLICATION_TO_CASEWORKER,
+    UNASSIGN_APPLICATION_TO_CASEWORKER
+}
