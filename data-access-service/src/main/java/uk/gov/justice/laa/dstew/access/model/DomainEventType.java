4d4d895ee1602f00bf1d542751ce4bb830281407
daniel.barnett <b4barnett@googlemail.com>
2025-12-08 16:18:15 +0000

    integration test

--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/model/DomainEventType.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.laa.dstew.access.model;
-
-/**
- * Represents a domain events type in the model.
- */
-public enum DomainEventType {
-    ASSIGN_APPLICATION_TO_CASEWORKER,
-    UNASSIGN_APPLICATION_TO_CASEWORKER
-}
