5afb00b8094824a0e032e454f866fa13906bfe2f
b4barnett <b4barnett@googlemail.com>
2025-12-12 13:43:26 +0000

    fix mapping so integration tests work (#108)

--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/entity/ApplicationSummaryEntity.java
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/entity/ApplicationSummaryEntity.java
@@ -51,7 +51,7 @@ public class ApplicationSummaryEntity {
 
   @ManyToMany(cascade = CascadeType.PERSIST)
   @JoinTable(
-          name = "linked_individual",
+          name = "linked_individuals",
           joinColumns = @JoinColumn(name = "application_id"),
           inverseJoinColumns = @JoinColumn(name = "individual_id")
   )
