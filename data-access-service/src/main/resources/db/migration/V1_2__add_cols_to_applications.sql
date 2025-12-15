5dbc0b15ed9fbbe9fb4b00c3ec8d108c48e9b86f
daniel.barnett <b4barnett@googlemail.com>
2025-12-15 12:39:09 +0000

    add new fields to entity

--- data-access-service/src/main/resources/db/migration/V1_2__add_cols_to_applications.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE public.applications
-ADD COLUMN apply_application_id UUID NOT NULL,
-ADD COLUMN submitted_at TIMESTAMPTZ NOT NULL,
-ADD COLUMN used_delegated_functions BOOLEAN NOT NULL,
-ADD COLUMN category_of_law VARCHAR,
-ADD COLUMN matter_types VARCHAR,
-ADD COLUMN is_auto_granted BOOLEAN NOT NULL;
