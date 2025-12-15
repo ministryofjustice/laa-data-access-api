87f91dc6fce1ff106f83ef9e17223ab2c715a19f
carl.mcintyre <carl.mcintyre@justice.gov.uk>
2025-12-15 17:05:44 +0000

    added not null checks for db generated fields

--- data-access-service/src/integrationTest/java/uk/gov/justice/laa/dstew/access/repository/ApplicationRepositoryTest.java
+++ data-access-service/src/integrationTest/java/uk/gov/justice/laa/dstew/access/repository/ApplicationRepositoryTest.java
@@ -27,5 +27,7 @@ public class ApplicationRepositoryTest extends BaseIntegrationTest {
                 .usingRecursiveComparison()
                 .ignoringFields("createdAt", "modifiedAt")
                 .isEqualTo(actual);
+        assertThat(expected.getCreatedAt()).isNotNull();
+        assertThat(expected.getModifiedAt()).isNotNull();
     }
 }
