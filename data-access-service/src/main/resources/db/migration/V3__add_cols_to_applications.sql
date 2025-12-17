e134904516cbf335d05c6673d25e492ee9305c6d
daniel.barnett <b4barnett@googlemail.com>
2025-12-17 12:53:29 +0000

    code review comments

--- data-access-service/src/main/resources/db/migration/V3__add_cols_to_applications.sql
+++ data-access-service/src/main/resources/db/migration/V3__add_cols_to_applications.sql
@@ -2,6 +2,6 @@ ALTER TABLE public.applications
 ADD COLUMN apply_application_id UUID NULL,
 ADD COLUMN submitted_at TIMESTAMPTZ NULL,
 ADD COLUMN used_delegated_functions BOOLEAN NULL,
-ADD COLUMN category_of_law VARCHAR,
-ADD COLUMN matter_types VARCHAR,
+ADD COLUMN category_of_law VARCHAR NULL,
+ADD COLUMN matter_types VARCHAR NULL,
 ADD COLUMN is_auto_granted BOOLEAN NULL;
