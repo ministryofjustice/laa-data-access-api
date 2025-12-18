6a4e97f6b1aa35d37ace1ace46015fab1437a46b
david-stuart-justice <David.Stuart@justice.gov.uk>
2025-12-18 15:07:52 +0000

    feat: DSTEW-950 added implementation that uses ResponseEntityExceptionHandlerAdvice for Bad Request failures

--- data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/controller/ApplicationControllerTest.java
+++ data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/controller/ApplicationControllerTest.java
@@ -3,19 +3,11 @@ package uk.gov.justice.laa.dstew.access.controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -36,9 +28,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.HttpStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -47,15 +36,15 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import uk.gov.justice.laa.dstew.access.exception.ResourceNotFoundException;
-import uk.gov.justice.laa.dstew.access.model.*;
 import uk.gov.justice.laa.dstew.access.model.Application;
 import uk.gov.justice.laa.dstew.access.model.ApplicationDomainEvent;
 import uk.gov.justice.laa.dstew.access.model.ApplicationSummary;
 import uk.gov.justice.laa.dstew.access.model.ApplicationSummaryResponse;
+import uk.gov.justice.laa.dstew.access.model.CaseworkerAssignRequest;
+import uk.gov.justice.laa.dstew.access.model.EventHistory;
 import uk.gov.justice.laa.dstew.access.service.ApplicationService;
 import uk.gov.justice.laa.dstew.access.service.ApplicationSummaryService;
 import uk.gov.justice.laa.dstew.access.service.DomainEventService;
-import uk.gov.justice.laa.dstew.access.specification.DomainEventSpecification;
 
 @WebMvcTest(
     controllers = ApplicationController.class,
@@ -193,15 +182,15 @@ class ApplicationControllerTest {
   @Test
   void shouldAssignCaseworker() throws Exception {
     var validRequest =
-    """
-    {
-      "caseworkerId" : "f67e5290-c774-4e13-809b-37fc6cf9b09b",
-      "applicationIds" : [
-        "33703b45-f8b7-4143-8b5d-969826bdd090",
-        "8b92afd8-ab7b-4f5b-b0ea-2dcd7c2cde8f"
-      ]
-    }
-    """;
+        """
+        {
+          "caseworkerId" : "f67e5290-c774-4e13-809b-37fc6cf9b09b",
+          "applicationIds" : [
+            "33703b45-f8b7-4143-8b5d-969826bdd090",
+            "8b92afd8-ab7b-4f5b-b0ea-2dcd7c2cde8f"
+          ]
+        }
+        """;
 
     mockMvc.perform(post("/api/v0/applications/assign")
                     .contentType("application/json")
@@ -230,7 +219,7 @@ class ApplicationControllerTest {
   }
 
   @Test
-  void shouldGetApplicationHistory() throws Exception{
+  void shouldGetApplicationHistory() throws Exception {
     final UUID applicationId = UUID.randomUUID();
 
     when(domainEventService.getEvents(applicationId, null))
@@ -250,26 +239,27 @@ class ApplicationControllerTest {
   @Test
   void shouldReturnNotFoundExceptionWithApplicationMessage() throws Exception {
 
+
+
+    UUID randomUuid = UUID.randomUUID();
+    when(applicationService.getApplication(randomUuid))
+            .thenThrow(new ResourceNotFoundException("Application not found"));
     OffsetDateTime createdAt = OffsetDateTime.now(ZoneOffset.UTC).minusDays(3);
     OffsetDateTime updatedAt = OffsetDateTime.now(ZoneOffset.UTC);
-
-    UUID randomUUID = UUID.randomUUID();
     var application = Application.builder()
-            .id(randomUUID)
+            .id(randomUuid)
             .laaReference("laa_reference")
             .applicationContent(Map.of("foo", "bar"))
             .createdAt(createdAt)
             .updatedAt(updatedAt)
             .build();
-    when(applicationService.getApplication(randomUUID)).thenThrow(new ResourceNotFoundException("Application not found"));
-    ProblemDetail expectedProblem = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
-    expectedProblem.setTitle("Not found");
-    expectedProblem.setDetail("Application not found");
     MvcResult result = mockMvc.perform(get("/api/v0/applications/" + application.getId()))
             .andExpect(status().is4xxClientError())
             .andExpect(content().contentType(MediaType.APPLICATION_PROBLEM_JSON))
             .andReturn();
-
+    ProblemDetail expectedProblem = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+    expectedProblem.setTitle("Not found");
+    expectedProblem.setDetail("Application not found");
 
     ProblemDetail problemDetail = deserialise(result, ProblemDetail.class);
     assertThat(problemDetail.getTitle()).isEqualTo(expectedProblem.getTitle());
@@ -287,17 +277,18 @@ class ApplicationControllerTest {
             UUID.fromString("8b92afd8-ab7b-4f5b-b0ea-2dcd7c2cde8f"));
     EventHistory eventHistory = new EventHistory();
     eventHistory.setEventDescription("Caseworker assigned");
+    doThrow(new ResourceNotFoundException("Caseworker not found"))
+            .when(applicationService).assignCaseworker(uuid, applicationIds, eventHistory);
+
+    ProblemDetail expectedProblem = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+    expectedProblem.setTitle("Not found");
+    expectedProblem.setDetail("Caseworker not found");
+
     CaseworkerAssignRequest request = new CaseworkerAssignRequest();
     request.setCaseworkerId(uuid);
     request.setApplicationIds(applicationIds);
     request.setEventHistory(eventHistory);
     String requestJson = objectMapper.writeValueAsString(request);
-
-
-    doThrow(new ResourceNotFoundException("Caseworker not found")).when(applicationService).assignCaseworker(uuid, applicationIds, eventHistory);
-    ProblemDetail expectedProblem = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
-    expectedProblem.setTitle("Not found");
-    expectedProblem.setDetail("Caseworker not found");
 
     MvcResult mvcResult = mockMvc.perform(post("/api/v0/applications/assign")
                     .contentType("application/json")
--- data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/exception/GlobalExceptionHandlerTest.java
+++ data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/exception/GlobalExceptionHandlerTest.java
@@ -2,12 +2,14 @@ package uk.gov.justice.laa.dstew.access.exception;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatException;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import uk.gov.justice.laa.dstew.access.validation.ValidationException;
 
@@ -37,12 +39,16 @@ class GlobalExceptionHandlerTest {
 
   @Test
   void handleValidationException_returnsBadRequestStatusAndErrors() {
-    var result = globalExceptionHandler.handleValidationException(new ValidationException(List.of("error1")));
+    ResponseEntity<ProblemDetail> result = globalExceptionHandler.handleValidationException(new ValidationException(List.of("error1")));
 
     assertThat(result).isNotNull();
-    assertThat(result.getStatusCode()).isEqualTo(BAD_REQUEST);
-    assertThat(result.getBody()).isNotNull();
-    assertThat(result.getBody().getProperties()).containsEntry("errors", List.of("error1"));
+    ProblemDetail problemDetail = result.getBody();
+    assertThat(problemDetail).isNotNull();
+    assertThat(problemDetail.getStatus()).isEqualTo(400);
+    assertThat(problemDetail.getTitle()).isEqualTo("Bad Request");
+    assertThat(problemDetail.getDetail()).isEqualTo("Generic Validation Error");
+    assertThat(problemDetail.getProperties()).isNotNull();
+    assertThat(problemDetail.getProperties()).containsEntry("errors", List.of("error1"));
   }
 
   @Test
@@ -52,12 +58,24 @@ class GlobalExceptionHandlerTest {
     assertThat(result).isNotNull();
     assertThat(result.getStatusCode()).isEqualTo(INTERNAL_SERVER_ERROR);
     assertThat(result.getBody()).isNotNull();
-    assertThat(result.getBody().getDetail()).isEqualTo("An unexpected application error has occurred.");
+    assertThat(result.getBody().getDetail()).isEqualTo("An unexpected error has occurred.");
   }
 
   @Test void handleAuthorizationDeniedException_throwsException_for_ExceptionTranslationFilter_to_handle() {
     assertThatException()
       .isThrownBy(() -> globalExceptionHandler.handleAuthorizationDeniedException(new AuthorizationDeniedException("")))
-      .isInstanceOf(AuthorizationDeniedException.class);
+        .isInstanceOf(AuthorizationDeniedException.class);
   }
+
+  @Test
+  void handleDataAccessException_returnsInternalServerErrorStatusAndErrorMessage() {
+    var result = globalExceptionHandler.handleDataAccessException(new DataRetrievalFailureException("Database error") {
+    });
+
+    assertThat(result).isNotNull();
+    assertThat(result.getStatusCode()).isEqualTo(INTERNAL_SERVER_ERROR);
+    assertThat(result.getBody()).isNotNull();
+    assertThat(result.getBody().getDetail()).isEqualTo("An unexpected error has occurred.");
+  }
+
 }
