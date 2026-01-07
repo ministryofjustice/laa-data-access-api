911138a5af81e5aca6c068d8c88b5770c4455d6b
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2026-01-07 16:15:37 +0000

    Added missing import

--- data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/service/DomainEventServiceTest.java
+++ data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/service/DomainEventServiceTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import static org.mockito.ArgumentMatchers.any;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
