9a51ace9b0bb14f4be493519334ae3ab23ca2817
carl.mcintyre <carl.mcintyre@justice.gov.uk>
2026-01-12 14:27:56 +0000

    Added CaseworkerRepositoryTest

---
+++ data-access-service/src/integrationTest/java/uk/gov/justice/laa/dstew/access/repository/CaseworkerRepositoryTest.java
@@ -0,0 +1,35 @@
+package uk.gov.justice.laa.dstew.access.repository;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.justice.laa.dstew.access.entity.ApplicationEntity;
+import uk.gov.justice.laa.dstew.access.entity.CaseworkerEntity;
+import uk.gov.justice.laa.dstew.access.utils.BaseIntegrationTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class CaseworkerRepositoryTest extends BaseIntegrationTest {
+
+    @Test
+    public void givenSaveOfExpectedCaseworker_whenGetCalled_expectedAndActualAreEqual() {
+
+        // given
+        CaseworkerEntity expected = caseworkerFactory.create();
+        caseworkerRepository.save(expected);
+
+        // when
+        CaseworkerEntity actual = caseworkerRepository.findById(expected.getId()).orElse(null);
+
+        // then
+        assertCaseworkerEqual(expected, actual);
+    }
+
+    private void assertCaseworkerEqual(CaseworkerEntity expected, CaseworkerEntity actual) {
+        assertThat(expected)
+                .usingRecursiveComparison()
+                .ignoringFields("createdAt", "modifiedAt")
+                .isEqualTo(actual);
+        assertThat(expected.getCreatedAt()).isNotNull();
+        assertThat(expected.getModifiedAt()).isNotNull();
+    }
+
+}
