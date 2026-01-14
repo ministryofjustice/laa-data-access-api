39167641a623340432ce54ebda2d1d9c7762d450
David Stuart <David.Stuart@justice.gov.uk>
2026-01-14 14:44:17 +0000

    DSTEW-991 change application validations to return a list of validation errors (#139)
    * feat: DSTEW-991 Change Application Validations to return a list of errors
    
    * feat: DSTEW-991 Updated open api spec and integration tests
    
    * feat: DSTEW-991 Refactor application validations to return a list of validation errors
    
    * feat: DSTEW-991 Removing validations and checks that will be captured by Jackson deserilization request level
    
    * feat: DSTEW-991 Simplify application validation tests and enhance error handling
    
    * feat: DSTEW-991 Updating tests following rebasing main

--- data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/validation/ApplicationValidationsTest.java
+++ data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/validation/ApplicationValidationsTest.java
@@ -1,33 +1,10 @@
 package uk.gov.justice.laa.dstew.access.validation;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import org.instancio.Instancio;
-import org.instancio.Select;
-import org.javers.common.collections.Arrays;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.justice.laa.dstew.access.model.ApplicationCreateRequest;
-import uk.gov.justice.laa.dstew.access.model.ApplicationStatus;
-import uk.gov.justice.laa.dstew.access.model.ApplicationUpdateRequest;
-import uk.gov.justice.laa.dstew.access.model.Individual;
 import uk.gov.justice.laa.dstew.access.shared.security.EffectiveAuthorizationProvider;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,8 +12,6 @@ public class ApplicationValidationsTest {
 
   @Mock
   EffectiveAuthorizationProvider mockEntra;
-  @Mock
-  IndividualValidations individualValidator;
 
   @InjectMocks
   ApplicationValidations classUnderTest;
