ce8e210292890b301daf85bf70ee6e42d9d12ac4
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2026-01-15 15:17:59 +0000

    chore: DSTEW-972 Add proceedings table (#140)

--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/entity/ProceedingsEntity.java
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/entity/ProceedingsEntity.java
@@ -2,8 +2,10 @@ package uk.gov.justice.laa.dstew.access.entity;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,53 +13,70 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.Instant;
+import java.util.Map;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import uk.gov.justice.laa.dstew.access.ExcludeFromGeneratedCodeCoverage;
-import uk.gov.justice.laa.dstew.access.enums.MeritsDecisionStatus;
+
 
 /**
- * Represents a merits decision.
+ * Represents a proceeding.
  */
 @ExcludeFromGeneratedCodeCoverage
+@Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder(toBuilder = true)
 @Entity
-@Table(name = "merits_decisions")
+@Table(name = "proceedings")
+@EntityListeners(AuditingEntityListener.class)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class MeritsDecisionEntity implements AuditableEntity {
+public class ProceedingsEntity implements AuditableEntity {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(columnDefinition = "UUID")
   private UUID id;
 
   @ManyToOne(optional = false)
-  @JoinColumn(name = "proceeding_id", nullable = false)
-  private ProceedingsEntity proceeding;
+  @JoinColumn(name = "application_id", nullable = false)
+  private ApplicationEntity application;
 
-  @Column(name = "created_at")
+  @Column(name = "apply_proceeding_id", nullable = false)
+  private UUID applyProceedingId;
+
+  @Column(name = "description", nullable = false)
+  private String description;
+
+  @Column(name = "is_lead", nullable = false)
+  private boolean isLead;
+
+  @Type(JsonType.class)
+  @Column(name = "proceeding_content", columnDefinition = "jsonb", nullable = false)
+  private Map<String, Object> proceedingContent;
+
   @CreationTimestamp
+  @Column(name = "created_at", nullable = false)
   private Instant createdAt;
 
-  @Column(name = "modified_at")
+  @Column(name = "created_by", nullable = false)
+  private String createdBy;
+
   @UpdateTimestamp
+  @Column(name = "modified_at", nullable = false)
   private Instant modifiedAt;
 
-  @Column(name = "decision")
-  private MeritsDecisionStatus decision;
-
-  @Column(name = "reason")
-  private String reason;
-
-  @Column(name = "justification")
-  private String justification;
+  @Column(name = "updated_by", nullable = false)
+  private String updatedBy;
 
   @Override
   public Instant getCreatedAt() {
@@ -66,7 +85,7 @@ public class MeritsDecisionEntity implements AuditableEntity {
 
   @Override
   public String getCreatedBy() {
-    return null;
+    return createdBy;
   }
 
   @Override
@@ -76,6 +95,6 @@ public class MeritsDecisionEntity implements AuditableEntity {
 
   @Override
   public String getUpdatedBy() {
-    return null;
+    return updatedBy;
   }
 }
