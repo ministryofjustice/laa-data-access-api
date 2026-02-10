e35442093041623a45a0ca3c98007c9be1ffcb3f
carl.mcintyre <carl.mcintyre@justice.gov.uk>
2026-02-10 13:46:21 +0000

    Added DateTimeHelper Ensured that submittedAt in ApplicationContentGenerator does not include the nanos part

--- data-access-service/src/testUtilities/java/uk/gov/justice/laa/dstew/access/utils/generator/application/ApplicationContentGenerator.java
+++ data-access-service/src/testUtilities/java/uk/gov/justice/laa/dstew/access/utils/generator/application/ApplicationContentGenerator.java
@@ -2,6 +2,8 @@ package uk.gov.justice.laa.dstew.access.utils.generator.application;
 
 import uk.gov.justice.laa.dstew.access.model.ApplicationContent;
 import uk.gov.justice.laa.dstew.access.utils.generator.BaseGenerator;
+import uk.gov.justice.laa.dstew.access.utils.helpers.DateTimeHelper;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -18,7 +20,7 @@ public class ApplicationContentGenerator extends BaseGenerator<ApplicationConten
         UUID applicationId = UUID.randomUUID();
         return ApplicationContent.builder()
                 .id(applicationId)
-                .submittedAt(Instant.now().toString())
+                .submittedAt(DateTimeHelper.GetSystemInstanceWithoutNanoseconds().toString())
                 .proceedings(List.of(proceedingDtoGenerator.createDefault()))
                 .build();
     }
