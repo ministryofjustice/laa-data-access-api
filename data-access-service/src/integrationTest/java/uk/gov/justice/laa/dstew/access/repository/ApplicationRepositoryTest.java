cb5203a24a00867475d9b4f9ba18f372e8c3bfc8
carl.mcintyre <carl.mcintyre@justice.gov.uk>
2026-02-11 19:32:22 +0000

    added full aggregate root to application repository test

--- data-access-service/src/integrationTest/java/uk/gov/justice/laa/dstew/access/repository/ApplicationRepositoryTest.java
+++ data-access-service/src/integrationTest/java/uk/gov/justice/laa/dstew/access/repository/ApplicationRepositoryTest.java
@@ -2,8 +2,16 @@ package uk.gov.justice.laa.dstew.access.repository;
 
 import org.junit.jupiter.api.Test;
 import uk.gov.justice.laa.dstew.access.entity.ApplicationEntity;
+import uk.gov.justice.laa.dstew.access.entity.DecisionEntity;
+import uk.gov.justice.laa.dstew.access.entity.ProceedingEntity;
 import uk.gov.justice.laa.dstew.access.utils.BaseIntegrationTest;
+import uk.gov.justice.laa.dstew.access.utils.generator.DataGenerator;
 import uk.gov.justice.laa.dstew.access.utils.generator.application.ApplicationEntityGenerator;
+import uk.gov.justice.laa.dstew.access.utils.generator.decision.DecisionEntityGenerator;
+import uk.gov.justice.laa.dstew.access.utils.generator.merit.MeritsDecisionsEntityGenerator;
+import uk.gov.justice.laa.dstew.access.utils.generator.proceeding.ProceedingsEntityGenerator;
+
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -13,7 +21,18 @@ public class ApplicationRepositoryTest extends BaseIntegrationTest {
     public void givenSaveOfExpectedApplication_whenGetCalled_expectedAndActualAreEqual() {
 
         // given
-        ApplicationEntity expected = persistedDataGenerator.createAndPersist(ApplicationEntityGenerator.class);
+        ApplicationEntity expected = persistedDataGenerator.createAndPersist(ApplicationEntityGenerator.class, builder ->
+                builder.caseworker(BaseIntegrationTest.CaseworkerJohnDoe));
+        ProceedingEntity proceeding = persistedDataGenerator.createAndPersist(ProceedingsEntityGenerator.class, builder -> {
+                builder.applicationId(expected.getId());
+        });
+        DecisionEntity expectedDecision = persistedDataGenerator.createAndPersist(DecisionEntityGenerator.class, builder -> {
+            builder.meritsDecisions(Set.of(DataGenerator.createDefault(MeritsDecisionsEntityGenerator.class, mBuilder -> {
+                mBuilder.proceeding(proceeding);
+            })));
+        });
+        expected.setDecision(expectedDecision);
+        applicationRepository.saveAndFlush(expected);
         clearCache();
 
         // when
