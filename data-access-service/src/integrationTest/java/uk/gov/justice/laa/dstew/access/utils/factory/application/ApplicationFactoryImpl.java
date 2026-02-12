01bd50e9368bce7af12fbc6c9ff4f91ab40c5c29
cmcintyre-moj <carl.mcintyre@justice.gov.uk>
2026-02-12 09:30:33 +0000

    DSTEW-1200 refactor application and caseworker integration tests (#185)

--- data-access-service/src/integrationTest/java/uk/gov/justice/laa/dstew/access/utils/factory/application/ApplicationFactoryImpl.java
+++ data-access-service/src/integrationTest/java/uk/gov/justice/laa/dstew/access/utils/factory/application/ApplicationFactoryImpl.java
@@ -8,7 +8,7 @@ import uk.gov.justice.laa.dstew.access.model.MatterType;
 import uk.gov.justice.laa.dstew.access.model.ApplicationStatus;
 import uk.gov.justice.laa.dstew.access.model.IndividualType;
 import uk.gov.justice.laa.dstew.access.utils.BaseIntegrationTest;
-import uk.gov.justice.laa.dstew.access.utils.DateTimeHelper;
+import uk.gov.justice.laa.dstew.access.utils.helpers.DateTimeHelper;
 import uk.gov.justice.laa.dstew.access.utils.factory.Factory;
 
 import java.time.InstantSource;
