973b449c3367301d181fc8679d2a0f35a3388223
shussain-justice <saif-ul.hussain@justice.gov.uk>
2026-02-25 16:19:37 +0000

    Add AllowApiAdmin

---
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/security/AllowApiAdmin.java
@@ -0,0 +1,17 @@
+package uk.gov.justice.laa.dstew.access.security;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+/**
+ * Security annotation that restricts access to methods or types
+ * to users with the "ADS_ADMIN" role.
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("@entra.hasAppRole('ADS_ADMIN')")
+public @interface AllowApiAdmin {
+}
