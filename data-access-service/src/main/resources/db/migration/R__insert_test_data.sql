89cea22d4f2361848c91030708f8303289af724e
sophie-moj <253530121+sophie-moj@users.noreply.github.com>
2026-02-25 11:37:37 +0000

    feat: dstew/1247 update caseworkers table with new caseworker IDs (#200)

--- data-access-service/src/main/resources/db/migration/R__insert_test_data.sql
+++ data-access-service/src/main/resources/db/migration/R__insert_test_data.sql
@@ -1,11 +1,11 @@
+Delete FROM Caseworkers;
+
 -- R__insert_test_data.sql
-INSERT INTO caseworkers (username) VALUES ('caseworker1');
-INSERT INTO caseworkers (username) VALUES ('caseworker2');
-INSERT INTO caseworkers (username) VALUES ('caseworker3');
-INSERT INTO caseworkers (username) VALUES ('caseworker4');
-INSERT INTO caseworkers (username) VALUES ('caseworker5');
-INSERT INTO caseworkers (username) VALUES ('caseworker6');
-INSERT INTO caseworkers (username) VALUES ('caseworker7');
-INSERT INTO caseworkers (username) VALUES ('caseworker8');
-INSERT INTO caseworkers (username) VALUES ('caseworker9');
-INSERT INTO caseworkers (username) VALUES ('caseworker10');
+INSERT INTO caseworkers (id, username) VALUES ('8a082fe2-d539-4177-aae3-7498fd5904c7', 'caseworker1');
+INSERT INTO caseworkers (id, username) VALUES ('41435bf3-625f-495a-ac1d-9bb95229b5a4', 'caseworker2');
+INSERT INTO caseworkers (id, username) VALUES ('afffcfe0-2b5b-40da-a8fc-5e31d724bb25', 'caseworker3');
+INSERT INTO caseworkers (id, username) VALUES ('b42db458-8740-456a-a3b2-75910785a9f2', 'caseworker4');
+INSERT INTO caseworkers (id, username) VALUES ('450bb6da-f06f-4251-a740-d015bea9b00c', 'caseworker5');
+INSERT INTO caseworkers (id, username) VALUES ('a291fbc4-146d-474d-a0c0-eae6a0ff04ce', 'caseworker6');
+INSERT INTO caseworkers (id, username) VALUES ('c5748122-6183-4255-aae0-a2916907b816', 'caseworker7');
+INSERT INTO caseworkers (id, username) VALUES ('0523e75e-139c-4355-8ef5-62aa5ee13b26', 'caseworker8');
