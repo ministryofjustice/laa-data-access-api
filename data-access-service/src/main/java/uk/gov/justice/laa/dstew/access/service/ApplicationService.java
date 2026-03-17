783d9a514a1b823437877260a71eab8db09331f9
david-stuart-justice <David.Stuart@justice.gov.uk>
2026-03-17 09:59:43 +0000

    feat: DSTEW-1306 add make decision granted domain event

--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/service/ApplicationService.java
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/service/ApplicationService.java
@@ -468,6 +468,7 @@ public class ApplicationService {
           .build();
 
       certificateRepository.save(certificate);
+      domainEventService.saveMakeDecisionGrantedDomainEvent(applicationId, request);
     }
 
     if (decision.getOverallDecision() == DecisionStatus.REFUSED) {
