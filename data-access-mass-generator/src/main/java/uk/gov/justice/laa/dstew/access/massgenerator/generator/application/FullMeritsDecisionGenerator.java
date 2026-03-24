f16d61575f553913dd2c2825196f9017996a655c
carl.mcintyre <carl.mcintyre@justice.gov.uk>
2026-03-24 21:04:33 +0000

    added decisions

---
+++ data-access-mass-generator/src/main/java/uk/gov/justice/laa/dstew/access/massgenerator/generator/application/FullMeritsDecisionGenerator.java
@@ -0,0 +1,23 @@
+package uk.gov.justice.laa.dstew.access.massgenerator.generator.application;
+
+import uk.gov.justice.laa.dstew.access.entity.MeritsDecisionEntity;
+import uk.gov.justice.laa.dstew.access.model.MeritsDecisionStatus;
+import uk.gov.justice.laa.dstew.access.utils.generator.BaseGenerator;
+
+public class FullMeritsDecisionGenerator
+        extends BaseGenerator<MeritsDecisionEntity, MeritsDecisionEntity.MeritsDecisionEntityBuilder> {
+
+    public FullMeritsDecisionGenerator() {
+        super(MeritsDecisionEntity::toBuilder, MeritsDecisionEntity.MeritsDecisionEntityBuilder::build);
+    }
+
+    @Override
+    public MeritsDecisionEntity createDefault() {
+        return MeritsDecisionEntity.builder()
+                .decision(faker.options().option(MeritsDecisionStatus.GRANTED, MeritsDecisionStatus.REFUSED))
+                .justification(faker.lorem().sentence())
+                .reason(faker.lorem().sentence())
+                .build();
+    }
+}
+
