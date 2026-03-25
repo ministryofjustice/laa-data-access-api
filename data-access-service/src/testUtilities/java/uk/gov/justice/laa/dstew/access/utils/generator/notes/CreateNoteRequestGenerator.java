3bca25262d28ad81e07111fbb315e1ffabe18c24
jason smallman <jason.smallman@digital.justice.gov.uk>
2026-03-25 14:21:27 +0000

    added testing

---
+++ data-access-service/src/testUtilities/java/uk/gov/justice/laa/dstew/access/utils/generator/notes/CreateNoteRequestGenerator.java
@@ -0,0 +1,19 @@
+package uk.gov.justice.laa.dstew.access.utils.generator.notes;
+
+import java.time.LocalDateTime;
+import uk.gov.justice.laa.dstew.access.model.CreateNoteRequest;
+import uk.gov.justice.laa.dstew.access.utils.generator.BaseGenerator;
+
+public class CreateNoteRequestGenerator extends BaseGenerator<CreateNoteRequest, CreateNoteRequest.Builder> {
+
+    public CreateNoteRequestGenerator() {
+        super(CreateNoteRequest::toBuilder, CreateNoteRequest.Builder::build);
+    }
+
+    @Override
+    public CreateNoteRequest createDefault() {
+        return CreateNoteRequest.builder()
+                .notes("this is a note, created on " + LocalDateTime.now())
+                .build();
+    }
+}
