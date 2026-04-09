e8014de201dd713d6be534c59c81ff535ef97fc5
Sophie Earl <253530121+sophie-moj@users.noreply.github.com>
2026-04-09 13:26:28 +0100

    fix test

--- data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/service/application/CreateApplicationTest.java
+++ data-access-service/src/test/java/uk/gov/justice/laa/dstew/access/service/application/CreateApplicationTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static uk.gov.justice.laa.dstew.access.service.application.sharedAsserts.ApplicationCreateRequestIndividualAssert.assertIndividualCollectionsEqual;
 
 import java.time.Instant;
 import java.util.ArrayList;
