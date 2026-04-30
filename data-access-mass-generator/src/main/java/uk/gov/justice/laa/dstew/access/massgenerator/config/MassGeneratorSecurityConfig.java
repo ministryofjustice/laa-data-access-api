f4f1bbbe2e558e06ef21f1af55d538c032a17f78
Sophie Earl <253530121+sophie-moj@users.noreply.github.com>
2026-04-30 17:34:30 +0100

    fix issue

--- data-access-mass-generator/src/main/java/uk/gov/justice/laa/dstew/access/massgenerator/config/MassGeneratorSecurityConfig.java
+++ data-access-mass-generator/src/main/java/uk/gov/justice/laa/dstew/access/massgenerator/config/MassGeneratorSecurityConfig.java
@@ -3,9 +3,9 @@ package uk.gov.justice.laa.dstew.access.massgenerator.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class MassGeneratorSecurityConfig {
@@ -14,7 +14,7 @@ public class MassGeneratorSecurityConfig {
   SecurityFilterChain massGeneratorSecurityFilterChain(HttpSecurity http) throws Exception {
     http.sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .csrf(AbstractHttpConfigurer::disable)
+        .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
     return http.build();
   }
