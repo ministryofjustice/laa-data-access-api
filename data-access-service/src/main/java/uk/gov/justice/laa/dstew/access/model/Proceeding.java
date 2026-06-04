6f091882115d8e9401778f7c26e4daae77a76e48
david-stuart-justice <David.Stuart@justice.gov.uk>
2026-06-04 10:00:06 +0100

    feat: DSTEW-1781 Updates after rebase

--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/model/Proceeding.java
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/model/Proceeding.java
@@ -57,6 +57,7 @@ public class Proceeding implements Serializable {
   @Nullable private LocalDate usedDelegatedFunctionsOn;
 
   @Nullable private String substantiveLevelOfServiceName;
+
   @Nullable private String substantiveLevelOfServiceNameEnum;
 
   @Nullable private Double substantiveCostLimitation;
