1b61943450f2563b27bab68acb846d12030df906
carl.mcintyre <carl.mcintyre@justice.gov.uk>
2026-06-05 15:59:06 +0100

    test: resolve coverage and exclude where needed

--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/exception/DomainEventPublishException.java
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/exception/DomainEventPublishException.java
@@ -1,6 +1,9 @@
 package uk.gov.justice.laa.dstew.access.exception;
 
+import uk.gov.justice.laa.dstew.access.ExcludeFromGeneratedCodeCoverage;
+
 /** The exception thrown when a domain event cannot be published. */
+@ExcludeFromGeneratedCodeCoverage
 public class DomainEventPublishException extends RuntimeException {
 
   /**
