1a75163813ab1ecd31ce16b06476e8e294c3fdcd
david-stuart-justice <David.Stuart@justice.gov.uk>
2026-07-10 14:58:12 +0100

    feat: DSTEW-1960 Rebased with latest refactors
    Signed-off-by: david-stuart-justice <David.Stuart@justice.gov.uk>

--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/controller/application/ApplicationQueryController.java
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/controller/application/ApplicationQueryController.java
@@ -18,22 +18,19 @@ import uk.gov.justice.laa.dstew.access.model.ApplicationOrderBy;
 import uk.gov.justice.laa.dstew.access.model.ApplicationResponse;
 import uk.gov.justice.laa.dstew.access.model.ApplicationSortBy;
 import uk.gov.justice.laa.dstew.access.model.ApplicationStatus;
-import uk.gov.justice.laa.dstew.access.model.ApplicationSummary;
 import uk.gov.justice.laa.dstew.access.model.ApplicationSummaryResponse;
 import uk.gov.justice.laa.dstew.access.model.DocumentDownloadResponse;
 import uk.gov.justice.laa.dstew.access.model.DomainEventType;
 import uk.gov.justice.laa.dstew.access.model.MatterType;
-import uk.gov.justice.laa.dstew.access.model.PagingResponse;
 import uk.gov.justice.laa.dstew.access.model.ServiceName;
-import uk.gov.justice.laa.dstew.access.service.applications.GetAllApplicationsService;
-import uk.gov.justice.laa.dstew.access.service.applications.GetCertificateService;
 import uk.gov.justice.laa.dstew.access.service.applications.SdsService;
 import uk.gov.justice.laa.dstew.access.service.domainevents.GetDomainEventService;
 import uk.gov.justice.laa.dstew.access.shared.logging.aspects.LogMethodArguments;
 import uk.gov.justice.laa.dstew.access.shared.logging.aspects.LogMethodResponse;
+import uk.gov.justice.laa.dstew.access.usecase.getallapplications.GetAllApplicationsUseCase;
 import uk.gov.justice.laa.dstew.access.usecase.getallnotesforapplication.GetAllNotesForApplicationUseCase;
 import uk.gov.justice.laa.dstew.access.usecase.getapplication.GetApplicationUseCase;
-import uk.gov.justice.laa.dstew.access.utils.PaginationHelper.PaginatedResult;
+import uk.gov.justice.laa.dstew.access.usecase.getcertificate.GetCertificateUseCase;
 
 /** Controller for handling /api/v0/applications query requests. */
 @RequiredArgsConstructor
@@ -43,12 +40,14 @@ public class ApplicationQueryController implements ApplicationQueryApi {
 
   private final GetApplicationUseCase getApplicationUseCase;
   private final GetApplicationResponseMapper getApplicationResponseMapper;
-  private final GetAllApplicationsService applicationSummaryService;
-  private final GetCertificateService certificateService;
   private final GetAllNotesForApplicationUseCase getAllNotesForApplicationUseCase;
   private final GetAllNotesForApplicationResponseMapper getAllNotesForApplicationResponseMapper;
   private final GetDomainEventService getDomainEventsService;
   private final SdsService sdsService;
+  private final GetAllApplicationsQueryMapper getAllApplicationsQueryMapper;
+  private final GetAllApplicationsResponseMapper getAllApplicationsResponseMapper;
+  private final GetAllApplicationsUseCase getAllApplicationsUseCase;
+  private final GetCertificateUseCase getCertificateUseCase;
 
   @Override
   @LogMethodResponse
@@ -68,33 +67,21 @@ public class ApplicationQueryController implements ApplicationQueryApi {
       Integer page,
       Integer pageSize) {
 
-    PaginatedResult<ApplicationSummary> result =
-        applicationSummaryService.getAllApplications(
-            status,
-            laaReference,
-            clientFirstName,
-            clientLastName,
-            clientDateOfBirth,
-            userId,
-            isAutoGranted,
-            matterType,
-            sortBy,
-            orderBy,
-            page,
-            pageSize);
-
-    List<ApplicationSummary> applications = result.page().stream().toList();
-    PagingResponse pagingResponse = new PagingResponse();
-    pagingResponse.setPage(result.requestedPage());
-    pagingResponse.pageSize(result.requestedPageSize());
-    pagingResponse.totalRecords((int) result.page().getTotalElements());
-    pagingResponse.itemsReturned(applications.size());
-
-    ApplicationSummaryResponse response = new ApplicationSummaryResponse();
-    response.setApplications(applications);
-    response.setPaging(pagingResponse);
-
-    return ResponseEntity.ok(response);
+    return getAllApplicationsResponseMapper.toGetAllApplicationsResponse(
+        getAllApplicationsUseCase.execute(
+            getAllApplicationsQueryMapper.toGetAllApplicationsQuery(
+                status,
+                laaReference,
+                clientFirstName,
+                clientLastName,
+                clientDateOfBirth,
+                userId,
+                isAutoGranted,
+                matterType,
+                sortBy,
+                orderBy,
+                page,
+                pageSize)));
   }
 
   @Override
@@ -130,7 +117,7 @@ public class ApplicationQueryController implements ApplicationQueryApi {
   @LogMethodResponse
   public ResponseEntity<Map<String, Object>> getCertificate(
       @NotNull ServiceName serviceName, UUID applicationId) {
-    return ResponseEntity.ok(certificateService.getCertificate(applicationId));
+    return ResponseEntity.ok(getCertificateUseCase.execute(applicationId).certificateContent());
   }
 
   @Hidden
