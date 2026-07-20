de1cb38ec6ce3583be63da73f53565e773d19198
shussain-justice <saif-ul.hussain@justice.gov.uk>
2026-07-20 10:15:48 +0100

    Move enums to domain (#397)

---
+++ data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/domain/enums/CategoryOfLaw.java
@@ -0,0 +1,6 @@
+package uk.gov.justice.laa.dstew.access.domain.enums;
+
+/** Domain enum representing the category of law for a legal aid application. */
+public enum CategoryOfLaw {
+  FAMILY
+}
--- data-access-service/src/main/java/uk/gov/justice/laa/dstew/access/usecase/shared/parser/CategoryOfLaw.java
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.laa.dstew.access.usecase.shared.parser;
-
-/** Usecase-level enum representing the category of law for a legal aid application. */
-public enum CategoryOfLaw {
-  FAMILY
-}
