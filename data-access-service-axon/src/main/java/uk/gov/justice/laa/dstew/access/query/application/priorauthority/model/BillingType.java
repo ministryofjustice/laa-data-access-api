50452d298a4caca6dd1cf4b04e07aa2867ccf310
David Stuart <248883171+david-stuart-justice@users.noreply.github.com>
2026-09-01 10:05:41 +0100

    feat: DSTEW-2240 implement get prior authority (#466)
    * feat: DSTEW-2241 Define Prior Authority get endpoint and response body in open-api spec. Added in minimal endpoint
    
    Signed-off-by: david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>
    
    * feat: DSTEW-2240 Get Prior Authority application
    
    Signed-off-by: david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>
    
    * feat: DSTEW-2240 Rename to get prior authorities
    
    Signed-off-by: david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>
    
    * feat: DSTEW-2240 Updated use cases to handle type being missing as this might not be set in a draft.
    
    Signed-off-by: david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>
    
    * feat: DSTEW-2240 Removed use of open api generated enums from the use case. Introduced mapping and enums
    
    Signed-off-by: david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>
    
    * feat: DSTEW-2240 File missed in previous commit
    
    Signed-off-by: david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>

---
+++ data-access-service-axon/src/main/java/uk/gov/justice/laa/dstew/access/query/application/priorauthority/model/BillingType.java
@@ -0,0 +1,14 @@
+package uk.gov.justice.laa.dstew.access.query.application.priorauthority.model;
+
+import uk.gov.justice.laa.dstew.access.ExcludeFromGeneratedCodeCoverage;
+
+/** Expert billing types supported by the get-prior-authority use case. */
+@ExcludeFromGeneratedCodeCoverage
+public enum BillingType {
+  HOURLY,
+  FIXED_RATE;
+
+  public static BillingType from(String value) {
+    return value == null ? null : valueOf(value);
+  }
+}
---
+++ data-access-service-axon/src/main/java/uk/gov/justice/laa/dstew/access/query/application/priorauthority/model/CounselType.java
@@ -0,0 +1,16 @@
+package uk.gov.justice.laa.dstew.access.query.application.priorauthority.model;
+
+import uk.gov.justice.laa.dstew.access.ExcludeFromGeneratedCodeCoverage;
+
+/** Counsel types supported by the get-prior-authority use case. */
+@ExcludeFromGeneratedCodeCoverage
+public enum CounselType {
+  KINGS_COUNSEL_ALONE,
+  TWO_JUNIOR_COUNSEL,
+  KINGS_COUNSEL_AND_JUNIOR_COUNSEL,
+  KINGS_COUNSEL_AND_TWO_JUNIOR_COUNSEL;
+
+  public static CounselType from(String value) {
+    return value == null ? null : valueOf(value);
+  }
+}
