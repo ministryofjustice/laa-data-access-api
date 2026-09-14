44704d86f6ab27f64ec261fad896cc5c3b332152
david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>
2026-09-14 10:00:27 +0100

    chore: Fix the CLI tool to handle new worklist assign pattern

--- data-access-tools/src/main/java/uk/gov/justice/laa/dstew/dataaccesstools/cli/applications/ApplicationCreationWorkflow.java
+++ data-access-tools/src/main/java/uk/gov/justice/laa/dstew/dataaccesstools/cli/applications/ApplicationCreationWorkflow.java
@@ -2,10 +2,13 @@ package uk.gov.justice.laa.dstew.dataaccesstools.cli.applications;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import uk.gov.justice.laa.dstew.dataaccesstools.utils.client.DataAccessApiClient;
 import uk.gov.justice.laa.dstew.dataaccesstools.utils.workflow.WorkflowResult;
 
 public final class ApplicationCreationWorkflow {
+  static final UUID DEFAULT_CASEWORKER_ID = UUID.fromString("8a082fe2-d539-4177-aae3-7498fd5904c7");
+
   private final DataAccessApiClient client;
   private final ApplicationRequestFactory applicationFactory;
   private final DecisionRequestFactory decisionFactory;
@@ -26,9 +29,14 @@ public final class ApplicationCreationWorkflow {
       try {
         client.createApplication(application.request());
         client.recordManualOutcome(application.applicationId());
+        client.assignWorkListItem(
+            application.applicationId(),
+            DEFAULT_CASEWORKER_ID,
+            0,
+            "Application assigned by data-access-tools for decision");
         client.makeDecision(
             application.applicationId(),
-            decisionFactory.create(application, decision, java.util.UUID.randomUUID()));
+            decisionFactory.create(application, decision, DEFAULT_CASEWORKER_ID));
         results.add(
             new WorkflowResult.ItemResult(
                 application.applicationId().toString(),
--- data-access-tools/src/main/java/uk/gov/justice/laa/dstew/dataaccesstools/utils/client/DataAccessApiClient.java
+++ data-access-tools/src/main/java/uk/gov/justice/laa/dstew/dataaccesstools/utils/client/DataAccessApiClient.java
@@ -3,7 +3,7 @@ package uk.gov.justice.laa.dstew.dataaccesstools.utils.client;
 import java.util.UUID;
 
 public interface DataAccessApiClient {
-  UUID createApplication(String requestBody);
+  void createApplication(String requestBody);
 
   void recordManualOutcome(UUID applicationId);
 
