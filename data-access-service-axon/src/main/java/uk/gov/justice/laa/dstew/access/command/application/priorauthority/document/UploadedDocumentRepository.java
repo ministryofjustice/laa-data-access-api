c01d2efa3e4b684cdfccac706ccb585da5f7d717
david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>
2026-09-24 12:35:23 +0100

    feat: Missing Javadoc checkstyle issue
    Signed-off-by: david-stuart-justice <248883171+david-stuart-justice@users.noreply.github.com>

--- data-access-service-axon/src/main/java/uk/gov/justice/laa/dstew/access/command/application/priorauthority/document/UploadedDocumentRepository.java
+++ data-access-service-axon/src/main/java/uk/gov/justice/laa/dstew/access/command/application/priorauthority/document/UploadedDocumentRepository.java
@@ -3,4 +3,5 @@ package uk.gov.justice.laa.dstew.access.command.application.priorauthority.docum
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/** Persistence interface for uploaded documents metadata. */
 public interface UploadedDocumentRepository extends JpaRepository<UploadedDocument, UUID> {}
